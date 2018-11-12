70d8e4e618ae9c66c38a7e5788a486f531659cb9
antoniomaxi <antonioguerra16@gmail.com>
2018-11-12 12:37:04 -0400

    ca

--- server.js
+++ server.js
@@ -24,13 +24,3 @@ console.log('Servidor corrienndo en puerto ');
 
 //get by Id
 
-app.get('eventos/:_id',function(req,res){
-    Evento.findById(req.params._id)
-    .then(eventoEncontrado => {
-        if(!eventoEncontrado){
-            return res.status(404).end();
-        }
-        return res.status(200).json(eventoEncontrado)
-    })
-    .catch(err => next(err))
-})
