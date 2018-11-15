889dfa0eab0e93a24e0fc0e367215aa6ce895df9
antoniomaxi <antonioguerra16@gmail.com>
2018-11-14 21:59:47 -0400

    moment js

---
+++ controllers/auth.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Usuario = require('../models/usuarios');
+
+function signUp(req,res){
+    const usuario = new Usuario({
+    
+    Nombre: req.body.Nombre,
+    Apellido: req.body.Apellido,
+    Correo: req.body.Correo,
+    Telefono: req.body.Telefono,
+    Imagen: req.body.Imagen,
+    Ciudad: req.body.Ciudad
+    })
+    usuario.save((err) =>{
+        if (err) res.status(500).send({message: `Error al crear el usuario: ${err}`} )
+
+        return res.status(200).send({token: service.createToken(usuario)})
+    })
+
+}
+
+function signIn(req,res){}
+
+
+module.exports = {
+
+    signIn,
+    signUp
+}
