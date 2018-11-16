0f62cd6d7390549c5ebaf34a092486d5c09badde
antoniomaxi <antonioguerra16@gmail.com>
2018-11-16 01:21:14 -0400

    token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ function isAuth(req,res,next){
 
         return res.status(403).send({message: `No tiene autorizacion`})
     }
-        const token = req.headers.authorization.split("")[1]
+        const token = req.headers.authorization
 
         services.decodeToken(token)
         .then(response =>{
