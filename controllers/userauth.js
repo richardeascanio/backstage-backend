4086140d5b3ab7717f06669f08a96a7af4de1884
antoniomaxi <antonioguerra16@gmail.com>
2018-11-17 21:05:34 -0400

    Prueba

--- controllers/userauth.js
+++ controllers/userauth.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const Usuario = require('../models/usuarios');
 const service = require('../services');
+const bcrypt = require('bcrypt-nodejs');
 
 function signUp(req,res){
     const usuario = new Usuario({
@@ -28,22 +29,35 @@ function Prueba(req,res){
 
 function signIn(req, res) {
   
-  Usuario.find({ Correo: req.params.Correo}, (err,usuario)=>{
+  Usuario.find({ Correo: req.params.Correo})
+    .then(usuario => {
     
-    if (err) return res.status(500)
-    .send({message: `Error al logearse el usuario: ${err}`})
+      if (err) return res.status(500)
+      .send({message: `Error al logearse el usuario: ${err}`})
 
-    if(!usuario)  return res.status(404)
-    .send({message: `No existe el usuario`})
+      if(!usuario)  return res.status(404)
+      .send({message: `No existe el usuario`})
 
-    req.usuario = usuario
+      req.usuario = usuario
 
-    res.status(200).
-    send({
-      message: 'Loggeado correctamente',
-      token: service.createToken(usuario) 
-    })
-  })
+      const password_verification = bcrypt.compareSync(  
+        req.body.Clave,
+        usuario.Clave
+      );
+
+      if (password_verification){
+
+        res.status(200).
+        send({
+          message: 'Loggeado correctamente',
+          token: service.createToken(usuario)
+        })
+      }else {
+
+        res.status(500).send({ message: 'Email o Contraseña incorrectos' });
+      }
+
+    })    
 }
 
 
