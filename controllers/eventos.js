a1797a1d2f77cadf17ce2d253a3303b3c6277bcf
antoniomaxi <antonioguerra16@gmail.com>
2018-11-27 10:03:01 -0400

    post

--- controllers/eventos.js
+++ controllers/eventos.js
@@ -14,6 +14,30 @@ function getEvento(req,res){
 
 }
 
+function saveEvento  (req,res){
+
+    let evento = new Eventos()
+    evento.Name = req.body.Name,
+    evento.Productor = req.body.Productor,
+    evento.Fecha = req.body.Fecha,
+    evento.Lugar = req.body.Lugar,
+    evento.Calificacion = req.body.Calificacion,
+    evento.Descripcion = req.body.Descripcion,
+    evento.Precio = req.body.Precio,
+    evento.Categoria = req.body.Categoria,
+    evento.Imagen = req.body.Imagen,
+    evento.Contador = req.body.Contador,
+    evento.Acumulador = req.body.Acumulador
+
+    evento.save((err,eventoStored)=>{
+
+        if(err) return res.json({message: `Error al guardar: ${err}`})
+
+        res.status(200).json({message: `Evento guardado: ${eventoStored}`})
+    })
+
+}
+
     function updateEvento(req, res){
         let _id = req.params._id
         let update = req.body
